b151ab2a3493e88026b039e6d3ac1453f2790291
typpo <ian@typpo.us>
2010-07-12 04:47:33 +0000

    added crypto files

---
+++ crypto.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int crypto_e(char *key, char *text)
+{
+    return 1;
+}
+
+int crypto_d(char *key, char *text)
+{
+    return 1;
+}
---
+++ crypto.h
@@ -0,0 +1,6 @@
+#ifndef CRYPTO_H
+#define CRYPTO_H
+int crypto_e(char *key, char *text);
+
+int crypto_d(char *key, char *text);
+#endif
